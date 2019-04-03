dfcac46fc2146efa4711ff28c3162f10fa1099e8
CLKBlu3 <lluis.marques@est.fib.upc.edu>
2019-04-03 22:55:24 +0200

    overlord scouting slightly improved

--- src/OverlordManager.cpp
+++ src/OverlordManager.cpp
@@ -2,12 +2,20 @@
 #include "BaseLocation.h"
 #include "GrezBot.h"
 #include "Util.h"
+#include "BaseLocationManager.h"
 
 
 const int distanceTreshold = 20;
 
 void OverlordManager::onFrame(GrezBot& bot) {
-
+	for (auto & unit : bot.UnitInfo().getUnits(Players::Self)) {
+		if (unit.getType().isOverlord()) {
+			if (unit.isIdle()) {
+				//make it do something
+				handleIdleOverlod(unit.getUnitPtr(), bot);
+			}
+		}
+	}
 }
 
 //Called From GrezBot OnUnitCreated, previoulsy Checked that it is an Overlord unit!!
@@ -26,14 +34,6 @@ void OverlordManager::OnUnitCreated(const sc2::Unit * unit, GrezBot & bot)
 			
 			std::vector<sc2::Point2D> destVector = { dest, returnPos };
 			
-			if (unit->health < (unit->health_max *0.75f)) {
-				//Scouting overloard has been harassed, back off?
-				if (enemyOnSight(unit->pos, bot)) { //back off
-					destVector.clear(); //clear orders
-					destVector.push_back(fleePos(bot)); //Return to base
-				}
-			}
-			
 			OverlordMove(unit, destVector, bot);
 		}
 }
@@ -50,6 +50,8 @@ void OverlordManager::OverlordMove(const sc2::Unit * unit, std::vector<sc2::Poin
 		if (enemyOnSight(unit->pos, bot)) { //back off
 			dest.clear(); //clear orders
 			dest.push_back(fleePos(bot)); //Return to base
+			bot.Actions()->UnitCommand(unit, sc2::ABILITY_ID::MOVE, fleePos(bot), true);
+			moved = true;
 		}
 	}
 	//Look if the unit orders contains the move order
@@ -79,6 +81,30 @@ bool OverlordManager::enemyOnSight(const sc2::Point2D pos, GrezBot& bot) {
 
 sc2::Point2D OverlordManager::fleePos(GrezBot& bot) {
 	return bot.Bases().getPlayerStartingBaseLocation(Players::Self)->getPosition();
+}
+
+void OverlordManager::handleIdleOverlod(const sc2::Unit* unit, GrezBot& bot)
+{
+	if (unit != nullptr) { //valid and idle Overlord
+		//move it to a new location
+		std::vector<sc2::Point2D> nextMove = bot.GetStartLocations();
+		sc2::Point2D nextExp;
+		for (int i = 0; i < 2; ++i) { //automatically add the nextExp of yours and enemy to the pool of possible routes
+			nextExp.x = bot.Bases().getNextExpansion(i).x;
+			nextExp.y = bot.Bases().getNextExpansion(i).y;
+			nextMove.push_back(nextExp);
+		}
+		addNextMove(unit, nextMove[rand()%nextMove.size()], bot);
+	}
+}
+
+void OverlordManager::addNextMove(const sc2::Unit* unit, const sc2::Point2D dest, GrezBot & bot) {
+	//Overlord may be idle or not!
+	if (unit->orders.empty()) {
+		//unit was idle!
+		std::vector<sc2::Point2D> move = { dest };
+		OverlordManager::OverlordMove(unit, move, bot);
+	}
 }
 
 
--- src/OverlordManager.h
+++ src/OverlordManager.h
@@ -6,9 +6,11 @@
 class GrezBot;
 
 namespace OverlordManager {
-	void onFrame(GrezBot & bot);
+	void onFrame(GrezBot& bot);
 	void OnUnitCreated(const sc2::Unit* unit, GrezBot& bot);
 	void OverlordMove(const sc2::Unit * unit, std::vector<sc2::Point2D> & dest, GrezBot& bot);
 	bool enemyOnSight(const sc2::Point2D pos, GrezBot& bot);
 	sc2::Point2D fleePos(GrezBot& bot);
+	void handleIdleOverlod(const sc2::Unit* unit, GrezBot& bot);
+	void addNextMove(const sc2::Unit* unit, const sc2::Point2D, GrezBot& bot);
 }
